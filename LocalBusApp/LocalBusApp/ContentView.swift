14a4890497eb139d381b6bae2a7ee5a164f2e1c5
Jong MIn <unib335@naver.com>
2026-01-14 09:10:13 +0900

    [Setting]: Xcode 프로젝트 초기 설정

---
+++ LocalBusApp/LocalBusApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalBusApp
+//
+//  Created by 이종민 on 1/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalBusApp/LocalBusApp/LocalBusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalBusAppApp.swift
+//  LocalBusApp
+//
+//  Created by 이종민 on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct LocalBusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
