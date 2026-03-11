b3b663bcb69e7ee8bfb5fb102089658f61ef498f
Jong MIn <unib335@naver.com>
2026-03-11 09:12:22 +0900

    [Design]: 개인정보처리방침 인앱 웹뷰 PrivacyPolicyView 추가

---
+++ LocalBusApp/LocalBusApp/Views/PrivacyPolicyView.swift
@@ -0,0 +1,78 @@
+import SwiftUI
+import WebKit
+
+// MARK: - WebView Wrapper
+
+private struct WebViewWrapper: UIViewRepresentable {
+    let url: URL
+    @Binding var isLoading: Bool
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(isLoading: $isLoading)
+    }
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+        webView.backgroundColor = UIColor.black
+        webView.scrollView.backgroundColor = UIColor.black
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+        @Binding var isLoading: Bool
+
+        init(isLoading: Binding<Bool>) {
+            _isLoading = isLoading
+        }
+
+        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+            isLoading = true
+        }
+
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            isLoading = false
+        }
+
+        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+            isLoading = false
+        }
+    }
+}
+
+// MARK: - 개인정보처리방침 화면
+
+struct PrivacyPolicyView: View {
+    @State private var isLoading = true
+
+    private let privacyURL = URL(string: "https://jongmini.github.io/LocalBus/privacy")!
+
+    var body: some View {
+        ZStack {
+            WebViewWrapper(url: privacyURL, isLoading: $isLoading)
+                .ignoresSafeArea(edges: .bottom)
+
+            if isLoading {
+                ProgressView()
+                    .tint(.white)
+            }
+        }
+        .navigationTitle("개인정보처리방침")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbarBackground(Color.black.opacity(0.95), for: .navigationBar)
+        .toolbarBackground(.visible, for: .navigationBar)
+        .toolbarColorScheme(.dark, for: .navigationBar)
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    NavigationStack {
+        PrivacyPolicyView()
+    }
+    .preferredColorScheme(.dark)
+}
